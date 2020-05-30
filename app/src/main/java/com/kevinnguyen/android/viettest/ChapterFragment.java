fba45bed06599f068bc9e5e9bd81c7d7a29f7c0b
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-05-29 22:59:24 -0400

    implemented Glide library to load images efficiently

--- app/src/main/java/com/kevinnguyen/android/viettest/ChapterFragment.java
+++ app/src/main/java/com/kevinnguyen/android/viettest/ChapterFragment.java
@@ -1,20 +1,30 @@
 package com.kevinnguyen.android.viettest;
-
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ChapterFragment extends Fragment {
     private RecyclerView mRecyclerView;
@@ -40,11 +50,14 @@ public class ChapterFragment extends Fragment {
     }
 
     private class ChapterHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "ChapterHolder";
         private Button mChapterButton;
+        private ImageView mImageView ;
 
         public ChapterHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.chapter_fragment, parent, false));
             mChapterButton = itemView.findViewById(R.id.chapter_button);
+            mImageView = itemView.findViewById(R.id.chapter_image);
             mChapterButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -52,11 +65,18 @@ public class ChapterFragment extends Fragment {
                     startActivity(intent);
                 }
             });
-
         }
 
-        public void bindChapter(String text) {
+        public void bindChapter(String text, int chapter) {
             mChapterButton.setText(text);
+            String chapterStr = "chapter" + (chapter + 1);
+            int drawable = Objects.requireNonNull(getActivity()).getResources().getIdentifier(chapterStr, "drawable",
+                    getActivity().getPackageName());
+            Log.d(TAG, "bindChapter: " + drawable);
+            Drawable image = getActivity().getResources().getDrawable(drawable);
+            Glide.with(getActivity())
+                    .load(image)
+                    .into(mImageView);
         }
     }
 
@@ -82,7 +102,7 @@ public class ChapterFragment extends Fragment {
         @Override
         public void onBindViewHolder(@NonNull ChapterHolder holder, int position) {
             Log.d(TAG, "onBindViewHolder: " + position);
-            holder.bindChapter("Chapter " + mChapter.get(position));
+            holder.bindChapter("Chapter " + mChapter.get(position), position);
         }
 
         @Override
